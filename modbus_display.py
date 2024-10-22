6dc63fbdd048bea05dcd256cd96df556f051ee2b
Santi <santiagocampojurado@gmail.com>
2024-10-22 13:10:02 +0200

    Save csv log file

--- modbus_display.py
+++ modbus_display.py
@@ -4,14 +4,20 @@ import drivers
 import minimalmodbus
 import time
 import datetime
+import csv
+import os
+
+
 
 display = drivers.Lcd()
+
 
 try:
     estacion = minimalmodbus.Instrument('/dev/ttyUSB0', 1)  # ttyUSB0
     estacion.serial.baudrate = 4800
     estacion.serial.timeout = None
 
+    #connection with the sensor
     test_value = estacion.read_register(0, 2)
     print(f"Comunicación establecida con éxito. Valor de prueba: {test_value}")
 
@@ -20,35 +26,61 @@ except Exception as e:
     exit(1)
 
 
+current_dir = os.getcwd()
+log_file_name = "log"
+log_path = os.path.join(current_dir, log_file_name)
+
+if not os.path.exists(log_path):
+    os.makedirs(log_path)
+
+fecha_comp_val = datetime.datetime.now()
+fecha_val = fecha_comp_val.strftime("%d-%m-%y")  # date in DD-MM-YY format
+hora_val = fecha_comp_val.strftime("%H-%M-%S")  # time in HH-MM-SS format
+filename = f"{log_path}/wind_{fecha_val}_{hora_val}.csv"
+
 
 def lectura():
-    try:
-        while True:
-            try:
-                # read wind speed, direction, and max speed
-                velocidad_val = estacion.read_register(0, 2)
-                direccion_val = estacion.read_register(1, 0)
-                maximo_val = estacion.read_register(2, 2)
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Fecha', 'Hora', 'Velocidad (m/s)', 'Dirección (grados)', 'Velocidad Máxima (m/s)'])
 
-                # date and time
-                fecha_comp_val = datetime.datetime.now()
-                fecha_val = fecha_comp_val.strftime("%d/%m/%y")
-                hora_val = fecha_comp_val.strftime("%H:%M:%S")
 
-                # display 
-                display.lcd_display_string(f"Vel: {velocidad_val}m/s",1)
-                display.lcd_display_string(f"Dir: {direccion_val}gd", 2)
+        try:
+            while True:
+                try:
+                    # read wind speed, direction, and max speed
+                    velocidad_val = estacion.read_register(0, 2)
+                    direccion_val = estacion.read_register(1, 0)
+                    maximo_val = estacion.read_register(2, 2)
 
-                print(f"{fecha_val} {hora_val} - Velocidad: {velocidad_val} m/s, Dirección: {direccion_val} grados, Máximo: {maximo_val} m/s")
-                time.sleep(1)
-                display.lcd_clear()
+                    # date and time
+                    fecha_comp_val = datetime.datetime.now()
+                    fecha_val = fecha_comp_val.strftime("%d/%m/%y")
+                    hora_val = fecha_comp_val.strftime("%H:%M:%S")
 
-            except Exception as e:
-                print(f"Error de lectura: {e}")
-                break
+                    display.lcd_display_string(f"Vel: {velocidad_val}m/s", 1)
+                    display.lcd_display_string(f"Dir: {direccion_val}grd", 2)
 
-    except KeyboardInterrupt:
-        print("Cleaning up!")
-        display.lcd_clear()
 
-lectura()
+                    print(f"{fecha_val} {hora_val} - Velocidad: {velocidad_val} m/s, Dirección: {direccion_val} grados, Máximo: {maximo_val} m/s")
+
+                    # data to csv
+                    writer.writerow([fecha_val, hora_val, velocidad_val, direccion_val, maximo_val])
+                    time.sleep(1)
+                    display.lcd_clear()
+
+                except Exception as e:
+                    print(f"Error de lectura: {e}")
+                    break
+
+        except KeyboardInterrupt:
+            print("Cleaning up!")
+            display.lcd_clear()
+
+
+def main():
+    lectura()
+
+
+if __name__ == "__main__":
+    main()
